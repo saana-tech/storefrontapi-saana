3c2ddf898d9f211877bbdaf5cf4ccf30ecb65192
jesus briceño <jesusebzcp19@gmail.com>
2021-04-29 10:24:56 -0500

    rafactor search

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -89,7 +89,7 @@ const NavBar = () => {
                   onClick={() => router.push("/")}
                 />
               </div>
-              <div style={{ margin: "0 128px" }} />
+              <div className="separatorNav" />
               <div className={styles.contentSelectAddress}>
                 <SelectAddress />
               </div>
