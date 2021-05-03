92c41664999c8aa1024e4f5c7896648c8d142383
Jesus briceño <jesusebzcp19@gmail.com>
2021-05-03 09:54:04 -0500

    add analitycs

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,11 +12,22 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="static/img/favicon.png" />
-
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-Y7M37QYJZ5`}
+          />
           <script
             async
             src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_API}&libraries=places`}
           ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `  window.dataLayer = window.dataLayer || [];
+                     function gtag(){dataLayer.push(arguments);}
+                     gtag('js', new Date());
+                     gtag('config', 'G-Y7M37QYJZ5');`,
+            }}
+          />
         </Head>
         <body>
           <Main />
