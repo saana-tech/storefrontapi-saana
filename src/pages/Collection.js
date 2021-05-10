31282b95503d2294d773eb20cc43d8446abb687b
Jesus briceño <jesusebzcp19@gmail.com>
2021-05-10 11:01:28 -0500

    default img

--- src/pages/Collection.js
+++ src/pages/Collection.js
@@ -93,7 +93,7 @@ const Collection = () => {
                 products.length > 0 &&
                 products.map(({ node }, index) => {
                   const { id, title, images, variants, description } = node;
-                  let imageUrl = images.edges[0].node.src;
+                  let imageUrl = images?.edges[0]?.node?.src;
                   let price = variants.edges[0].node.price;
                   let variantId = variants.edges[0].node.id;
                   let sku = variants.edges[0].node.sku;
