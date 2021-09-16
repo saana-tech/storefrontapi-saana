e0d6a740dd6cba32214c7cc8de9d603561aff314
Jesusebz <48937476+jesusebzcp@users.noreply.github.com>
2021-09-16 12:49:30 -0500

    refactor

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,5 @@
 import React, { useCallback, useContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { useRouter } from "next/router";
 import jwt from "jsonwebtoken";
 
 import { StoreContext } from "../../core";
@@ -19,9 +18,6 @@ import { getAffiliationsPackages } from "../../core/packages/actions";
 import Loading from "../Loading";
 
 const Layout = ({ children }) => {
-  const router = useRouter();
-  let { t: tokenParams } = router?.query;
-
   const { state, globalDispatch, authDispatch, packageDispatch } =
     useContext(StoreContext);
   const { authState, globalState } = state;
@@ -60,18 +56,7 @@ const Layout = ({ children }) => {
     } catch (error) {
       console.log("error", error);
     }
-  }, [token, tokenParams]);
-
-  const loginSubscriptionParams = useCallback(() => {
-    try {
-      if (tokenParams) {
-        const { id = "" } = jwt.verify(tokenParams, KEY_SECRET);
-        getUserDispatch(id, authDispatch);
-      }
-    } catch (error) {
-      console.log("error:loginSubscriptionParams", error);
-    }
-  }, [tokenParams]);
+  }, [token]);
 
   const handleSearchPackage = useCallback(async () => {
     if (user) {
@@ -84,10 +69,6 @@ const Layout = ({ children }) => {
   useEffect(() => {
     loginSubscription();
   }, [loginSubscription]);
-
-  useEffect(() => {
-    loginSubscriptionParams();
-  }, [loginSubscriptionParams]);
 
   useEffect(() => {
     handleSearchPackage();
